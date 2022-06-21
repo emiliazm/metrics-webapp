a9919f5fcc4f5891393197745a65748a8716986b
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-06-21 23:19:31 +0200

    Create pages

---
+++ src/pages/DetailsPage.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import CountriesList from '../components/CountriesList';
+
+const DetailsPage = () => (
+  <div className="container">
+    <CountriesList />
+  </div>
+);
+
+export default DetailsPage;
