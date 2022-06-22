eb339766a2bb9360e2876b5c623bf5933ddffe82
emiliazm <98142626+emiliazm@users.noreply.github.com>
2022-06-22 14:57:29 +0200

    Add useEffect to dispatch continent

--- src/components/CountriesList.js
+++ src/components/CountriesList.js
@@ -1,20 +1,26 @@
 import { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
+import { useParams } from 'react-router-dom';
 import { getCountries } from '../redux/countries/countries';
+import { getContinent } from '../redux/continents/continents';
 import Country from './Country';
 
 function CountriesList() {
+  const { continentName } = useParams();
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getContinent(continentName));
+  }, [continentName]);
+
   const { countries } = useSelector((state) => state.continent);
   const countriesString = countries?.toString();
-  console.log('countries', countries, countriesString);
-
-  const dispatch = useDispatch();
 
   useEffect(() => {
     if (countriesString) {
       dispatch(getCountries(countriesString));
     }
-  }, []);
+  }, [countriesString]);
 
   const countriesData = useSelector((state) => state.countries);
 
